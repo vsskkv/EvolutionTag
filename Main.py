a46034c2ef185f191fe16b459cb40b13eda76500
Vikram <vsskkv@gmail.com>
2021-02-10 18:38:13 +0000

    NF - 2D grid terminal based

--- Main.py
+++ Main.py
@@ -1,5 +1,17 @@
-#Date: 01/12/2020
+#Date: 12/11/2020
 #Author: Vikram Singh Kainth
 #Title: Main file
 #About: To be the main file where the program will initially run
 
+# define the row = 10 and cols = 10, so 100 square grid
+rows, cols = (10, 10)
+
+# make grid
+grid = [[0]*cols]*rows
+
+# place bot for testing 
+grid [0][0] = "bot"
+
+#display grid in terminal
+for row in grid:
+    print(row)
